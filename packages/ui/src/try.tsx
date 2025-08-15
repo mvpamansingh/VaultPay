7f95717a2818f418718ecb117aa45b1bef1163e1
mvpamansingh <amancodes5@gmail.com>
2025-08-15 12:49:03 +0530

    added tailwind CSS V4 in app

---
+++ packages/ui/src/try.tsx
@@ -0,0 +1,10 @@
+function Try()
+{
+    return(
+        <div className="bg-gray-300 w-50 h-100 text-red-500 text-2xl ">
+            This is a try component from helper pac
+        </div>
+    )
+}
+
+export {Try}
